f4b8b0cb5fbd647577e911db6e448e5a0e5376ec
walle <zlkjzxj@163.com>
2019-01-25 17:23:50 +0800

    create the fream

---
+++ src/main/java/com/sunny/sunnyfream/SunnyfreamApplication.java
@@ -0,0 +1,14 @@
+package com.sunny.sunnyfream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunnyfreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SunnyfreamApplication.class, args);
+	}
+
+}
+
